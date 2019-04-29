aeca370f0541433a48ae4a5fcc4c0401a554885e
Kelvin Cortez <kelvcortez@gmail.com>
2019-04-29 13:55:47 +1200

    Add project files.

---
+++ AimPhotography/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AimPhotography.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AimPhotography/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AimPhotography.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
