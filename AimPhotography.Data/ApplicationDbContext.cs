43a8fb14db292724685690da6c07749d9cf2c1be
Kelvin Cortez <kelvcortez@gmail.com>
2019-04-29 14:12:10 +1200

    Setting up Data foler. Initial commit

--- AimPhotography.Data/ApplicationDbContext.cs
+++ AimPhotography.Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using AimPhotography.Models;
+using AimPhotography.Data.Models;
 
 namespace AimPhotography.Data
 {
@@ -13,14 +13,6 @@ namespace AimPhotography.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
